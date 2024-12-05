9533f9ce3f9b6287df3c251a51981d1918591075
Nikita Menshov <nikitatop2222@gmail.com>
2024-12-05 13:06:23 +0500

    Update src/bot/main.py

--- src/bot/main.py
+++ src/bot/main.py
@@ -3,7 +3,7 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
 # routers 
-from .handlers.start import router as start_rt
+from .handlers.command import router as start_rt
 
 # Other
 from .config import TOKEN
@@ -16,16 +16,3 @@ async def main() -> None:
     dp.include_router(start_rt)
 
     await dp.start_polling(bot)
-
-
-async def stop_bot():
-    try:
-        if dp._polling:
-            await dp.stop_polling()
-
-        await bot.session.close()
-        await dp.storage.close()
-        print("Bot stopped successfully.")
-    except Exception as e:
-        print(f"Error stopping bot: {e}")
-
